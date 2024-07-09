03470f2c20da45b2b7fbee256396da5c99feda0d
sandyspiers <sandy.spiers@curtin.edu.au>
2024-07-09 11:22:41 +0800

    change to Integer type

--- src/partitioner.jl
+++ src/partitioner.jl
@@ -1,10 +1,10 @@
 """
-    ispartition(partitions, elements::Int)
+    ispartition(partitions, elements::Integer)
 
 Is the provided partition set a valid set (of depths at most 1)
 of the number of elements?
 """
-function ispartition(partitions, elements::Int)::Bool
+function ispartition(partitions, elements::Integer)::Bool
     # keep track of remaining elements
     remaining_elements = collect(1:elements)
     # flatten the partitions (should only be depths at most 1)
@@ -22,18 +22,8 @@ function ispartition(partitions, elements::Int)::Bool
     return true
 end
 
-"""
-    build_edms(locations::Matrix{T}, partition) where {T<:Real}
-
-Uses a partition set to build a set of **sqaured EDMs** of the given locations,
-where the partition sets describes which coordinates to use.
-"""
-function build_edms(locations::Matrix{T}, partition) where {T<:Real}
-    return [build_edm(locations[:, par]) .^ 2 for par in partition]
-end
-
 # Return a partition set determined by evals and with a given strategy
-function partition(evals::Vector{T}, num::Int; strategy::String="all") where {T<:Real}
+function partition(evals::Vector{T} where {T<:Real}, num::Integer; strategy::String="all")
     # TODO: Provide other strategies
     coords = length(evals)
     if strategy == "random"
@@ -51,4 +41,14 @@ function partition(evals::Vector{T}, num::Int; strategy::String="all") where {T<
         return partitions
     end
     throw(ArgumentError("$strategy is not a valid partition strategy"))
+end
+
+"""
+    build_edms(locations::Matrix{T}, partition) where {T<:Real}
+
+Uses a partition set to build a set of **sqaured EDMs** of the given locations,
+where the partition sets describes which coordinates to use.
+"""
+function build_edms(locations::Matrix{T}, partition) where {T<:Real}
+    return [build_edm(locations[:, par]) .^ 2 for par in partition]
 end
