12d8b6252b307a1006d8bf76666ceb59d4e449f2
sandyspiers <sandy.spiers@curtin.edu.au>
2024-07-09 11:39:43 +0800

    change test structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,15 @@
+using CoordinatePartitioning:
+    FLOAT_TOL,
+    STRATEGIES,
+    build_edm,
+    build_edms,
+    euclid_embed,
+    grammian,
+    isedm,
+    ispartition,
+    partition
+using Test
+
+include("test_edms.jl")
 include("test_partitioner.jl")
+include("test_modeler.jl")
